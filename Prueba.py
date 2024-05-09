00124b0f9eb6bb215d672e86aceac8d48e09de22
Maariioms <mora.sant.mario@gmail.com>
2024-05-09 08:24:57 -0600

    Sexta Vez

--- Prueba.py
+++ Prueba.py
@@ -1,3 +1,9 @@
 print("Adios Mundo")
 # git push origin main - para sync sin boton
 # git pull origin main - Actualizarlo
+
+"""
+1 - git status -> Para ver si esta actualizado o no
+2 - git add . -> Añade todo / git add 1.py 2.py
+3 - git commit -m "Comentario"
+"""
